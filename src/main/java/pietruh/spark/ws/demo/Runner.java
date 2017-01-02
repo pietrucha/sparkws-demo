20aead42ed6d191474faa4d2890ab53925000495
ppietrucha <pietruchpawel@gmail.com>
2017-01-02 16:43:00 +0100

    rest changes

--- src/main/java/pietruh/spark/ws/demo/Runner.java
+++ src/main/java/pietruh/spark/ws/demo/Runner.java
@@ -1,28 +1,61 @@
 package pietruh.spark.ws.demo;
 
-import static spark.Spark.*;
-
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import pietruh.spark.ws.demo.model.Line;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static spark.Spark.*;
 
 /**
  * Created by ppietrucha on 2016-12-16.
  */
 public class Runner {
 
-	public static void main(String[] args) {
-		port(9090);
-		// staticFileLocation("/public"); //index.html is served at
-		// localhost:4567 (default port)
-		webSocket("/chat", WebSocketHandler.class);
-		get("/line", (req, res) -> {
-			Gson gson = new Gson();
-			gson.toJson(src)
-			return null;
-		});
-		init();
-	}
+    public static void main(String[] args) {
+        port(9090);
+        // staticFileLocation("/public"); //index.html is served at
+        // localhost:4567 (default port)
+        webSocket("/chat", WebSocketHandler.class);
+        get("/lines/:id", (req, res) -> {
+            try {
+                GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+                String id = req.params(":id");
+
+                Coordinate[] coords =
+                      new Coordinate[] { new Coordinate(49.75177, 21.48067), new Coordinate(49.74534, 21.47338), new Coordinate(49.7419, 21.46857),
+                            new Coordinate(49.73713, 21.46634) };
+
+                LineString line = geometryFactory.createLineString(coords);
+                Line l = new Line();
+                l.setName(id);
+                l.setGeometry(line);
+                l.setCoordinates(line.getCoordinates());
+                Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
+                String s1 = gson.toJson(l, Line.class);
+                return s1;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+        get("/lines", (req, res) -> {
+            Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
+            List<Line> lines = IntStream.range(0, 10).mapToObj(i -> {
+                Line l = new Line();
+                l.setName("" + i);
+                return l;
+            }).collect(Collectors.toList());
+
+            return gson.toJson(lines);
+        });
+        init();
+    }
 }
--- src/main/java/pietruh/spark/ws/demo/model/Line.java
+++ src/main/java/pietruh/spark/ws/demo/model/Line.java
@@ -1,31 +1,41 @@
 package pietruh.spark.ws.demo.model;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.LineString;
 
 public class Line {
 
-	private String name;
-	private LineString geometry;
+    private String name;
+    private LineString geometry;
+    private Coordinate coordinates[];
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public LineString getGeometry() {
-		return geometry;
-	}
+    public LineString getGeometry() {
+        return geometry;
+    }
 
-	public void setGeometry(LineString geometry) {
-		this.geometry = geometry;
-	}
+    public void setGeometry(LineString geometry) {
+        this.geometry = geometry;
+    }
 
-	@Override
-	public String toString() {
-		return "Line [name=" + name + ", geometry=" + geometry + "]";
-	}
+    public Coordinate[] getCoordinates() {
+        return this.coordinates;
+    }
+
+    public void setCoordinates(Coordinate[] coords) {
+        this.coordinates = coords;
+    }
+
+    @Override
+    public String toString() {
+        return "Line [name=" + name + ", geometry=" + geometry + "]";
+    }
 
 }
