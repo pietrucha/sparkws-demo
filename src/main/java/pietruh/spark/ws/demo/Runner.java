abddc54f60edfd5bea52169f25dbbe1c9d391945
ppietrucha <pietruchpawel@gmail.com>
2017-01-03 12:48:07 +0100

    add CORS to REST

--- src/main/java/pietruh/spark/ws/demo/Runner.java
+++ src/main/java/pietruh/spark/ws/demo/Runner.java
@@ -3,10 +3,10 @@ package pietruh.spark.ws.demo;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import org.geotools.geometry.jts.JTSFactoryFinder;
 import pietruh.spark.ws.demo.model.Line;
+import spark.Route;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,33 +19,35 @@ import static spark.Spark.*;
  */
 public class Runner {
 
+    private Runner() {
+
+    }
+
     public static void main(String[] args) {
+
         port(9090);
         // staticFileLocation("/public"); //index.html is served at
         // localhost:4567 (default port)
         webSocket("/chat", WebSocketHandler.class);
-        get("/lines/:id", (req, res) -> {
+        Route route = (req, res) -> {
             try {
-                GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
-                String id = req.params(":id");
-
                 Coordinate[] coords =
                       new Coordinate[] { new Coordinate(49.75177, 21.48067), new Coordinate(49.74534, 21.47338), new Coordinate(49.7419, 21.46857),
                             new Coordinate(49.73713, 21.46634) };
 
-                LineString line = geometryFactory.createLineString(coords);
+                LineString line = JTSFactoryFinder.getGeometryFactory().createLineString(coords);
                 Line l = new Line();
-                l.setName(id);
+                l.setName(req.params(":id"));
                 l.setGeometry(line);
                 l.setCoordinates(line.getCoordinates());
                 Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
-                String s1 = gson.toJson(l, Line.class);
-                return s1;
+                return gson.toJson(l, Line.class);
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
-        });
+        };
+        get("/lines/:id", route);
         get("/lines", (req, res) -> {
             Gson gson = new GsonBuilder().setPrettyPrinting().serializeSpecialFloatingPointValues().create();
             List<Line> lines = IntStream.range(0, 10).mapToObj(i -> {
@@ -56,6 +58,29 @@ public class Runner {
 
             return gson.toJson(lines);
         });
+
+        options("/*",
+              (request, response) -> {
+
+                  String accessControlRequestHeaders = request
+                        .headers("Access-Control-Request-Headers");
+                  if (accessControlRequestHeaders != null) {
+                      response.header("Access-Control-Allow-Headers",
+                            accessControlRequestHeaders);
+                  }
+
+                  String accessControlRequestMethod = request
+                        .headers("Access-Control-Request-Method");
+                  if (accessControlRequestMethod != null) {
+                      response.header("Access-Control-Allow-Methods",
+                            accessControlRequestMethod);
+                  }
+
+                  return "OK";
+              });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
         init();
     }
+
 }
